e94a6bc1a2ede8bed00fe8cf585430b291af77e9
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-04 18:26:02 +0100

    confronto risultati

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -9,4 +9,14 @@ const dadoComputer = Math.floor(Math.random() * 6) + 1;
 // stampo i risultati
 
 console.log("Giocatore", dadoGiocatore);
-console.log("computer", dadoComputer);
+console.log("Computer", dadoComputer);
+
+// confronto i risultati
+
+if (dadoGiocatore > dadoComputer) {
+  console.log("Vince il Giocatore");
+} else if (dadoGiocatore < dadoComputer) {
+  console.log("Vince il Computer");
+} else {
+  console.log("Pareggio");
+}
