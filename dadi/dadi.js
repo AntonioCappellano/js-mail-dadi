7f285149c5aa90217c874f290518d3995e4dea74
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-04 18:17:16 +0100

    random number Computer

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,3 +1,7 @@
 // numero random per il giocatore
 
 const dadoGiocatore = Math.floor(Math.random() * 6) + 1;
+
+// numero random per il computer
+
+const dadoComputer = Math.floor(Math.random() * 6) + 1;
