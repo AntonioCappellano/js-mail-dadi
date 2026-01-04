35014db0a13c9ffc5f352473a239f89b0d5ebb4c
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-04 18:18:42 +0100

    Stamp result

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -5,3 +5,8 @@ const dadoGiocatore = Math.floor(Math.random() * 6) + 1;
 // numero random per il computer
 
 const dadoComputer = Math.floor(Math.random() * 6) + 1;
+
+// stampo i risultati
+
+console.log("Giocatore", dadoGiocatore);
+console.log("computer", dadoComputer);
